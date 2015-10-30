8003a09420acca2b7e82c37faa86eb3ef410ea22
dewise87 <daniel.wise@chicagobooth.edu>
2015-10-29 19:50:11 -0500

    HW5: Photogram

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,42 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    @photo = Photo.new
+    @photo.caption = params[:caption]
+    @photo.source = params[:source]
+    @photo.save
+
+    redirect_to("http://localhost:3000")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def update_row
+    @photo = Photo.find_by({ :id => params[:id]})
+
+    @photo.caption = params[:caption]
+    @photo.source = params[:source]
+    @photo.save
+
+    render("show")
+  end
+
+  def destroy
+    p = Photo.find_by({ :id => params[:id]})
+    p.destroy
+    p.save
+
+    redirect_to("http://localhost:3000")
+  end
 end
